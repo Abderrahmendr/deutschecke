47b43fdd6e394b1fc155960717e3a2b70fadee1b
Bileldr <dodoabdou324@gmail.com>
2025-05-20 23:44:22 +0100

    change

--- src/_Components/Navbar/Navbar.jsx
+++ src/_Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuIt
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
+  { name: 'Cources', href: '#', current: true },
   { name: 'Team', href: '#', current: false },
   { name: 'Projects', href: '#', current: false },
   { name: 'Calendar', href: '#', current: false },
