25aeb0dd33e6c108079fb35fcd14f5f94af526d0
Abderrahmendr <dorbaneabderrahmen@gmail.com>
2025-05-20 23:59:25 +0100

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './_Components/Navbar/Navbar'
+import Hero from './_Components/Hero/Hero'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
